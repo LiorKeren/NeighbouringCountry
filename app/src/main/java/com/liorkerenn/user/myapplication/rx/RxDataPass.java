ed48c85a5c025eaee0a96df8491e7bc194931805
Lior K <Liorkerenn@gmail.com>
2021-05-13 06:16:40 +0300

    fix github bug

--- app/src/main/java/com/liorkerenn/user/myapplication/rx/RxDataPass.java
+++ app/src/main/java/com/liorkerenn/user/myapplication/rx/RxDataPass.java
@@ -16,8 +16,6 @@ public class RxDataPass {
 
     private BehaviorSubject<Country> behaviorSubject;
 
-    private BehaviorSubject<String> itemEditTextOnTextChangedSubject;
-
     private BehaviorSubject<Class> startActivitySubject;
 
     //Subscribe a string
@@ -30,25 +28,9 @@ public class RxDataPass {
         return behaviorSubject;
     }
 
-    //Subscribe a string
-    public BehaviorSubject<String> getOnTextChangedSubject() {
-        return itemEditTextOnTextChangedSubject;
-    }
-
-    private BehaviorSubject<Object> behaviorSubjectObject;
-
-    public BehaviorSubject<Object> getObjectSubject() {
-        return behaviorSubjectObject;
-    }
-
-
     @Inject
     public RxDataPass() {
         behaviorSubject
-                = BehaviorSubject.create();
-        itemEditTextOnTextChangedSubject
-                = BehaviorSubject.create();
-        behaviorSubjectObject
                 = BehaviorSubject.create();
         startActivitySubject
                 = BehaviorSubject.create();
